8f3acc498d738936e1291dc7aea9fedc50cb7160
lighter <apple.xva@gmail.com>
2019-06-28 22:45:50 +0800

    First commit

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'ichat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ichat
+
+  pod 'Firebase/Database'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Firestore'
+
+  pod 'ProgressHUD'
+  pod 'MBProgressHUD'
+  pod 'IQAudioRecorderController'
+
+  pod 'JSQMessagesViewController', '7.3.3'
+  pod 'IDMPhotoBrowser'
+
+  pod 'ImagePicker'
+
+end
---
+++ ichat/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  icaht
+//
+//  Created by lighter on 2019/6/9.
+//  Copyright © 2019 lighter. All rights reserved.
+//
+
+import UIKit
+import ProgressHUD
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPassworTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: IBAction
+    @IBAction func login(_ sender: Any) {
+        dismissKeyboard()
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            login()
+        } else {
+            ProgressHUD.showError("Email and Password is missing!")
+        }
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        dismissKeyboard()
+        
+        if emailTextField.text != "" && passwordTextField.text != "" && repeatPassworTextField.text != "" {
+            
+            if passwordTextField.text == repeatPassworTextField.text {
+                register()
+            } else {
+                ProgressHUD.showError("Password don't match")
+            }
+
+        } else {
+            ProgressHUD.showError("All fields are required!")
+        }
+    }
+    
+    @IBAction func backgroundTap(_ sender: Any) {
+    }
+    
+    // MARK: HelperFunctions
+    func login() {
+        ProgressHUD.show("Login...")
+        
+        FUser.loginUserWith(email: emailTextField.text!, password: passwordTextField.text!) { (error) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            
+            self.goToApp()
+        }
+    }
+    
+    func register() {
+    
+    }
+    
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func cleanTextField() {
+        emailTextField.text = ""
+        passwordTextField.text = ""
+        repeatPassworTextField.text = ""
+    }
+    
+    // MARK: GoToApp
+    func gotToApp() {
+        ProgressHUD.dismiss()
+        cleanTextField()
+        dismissKeyboard()
+        
+        // present app here
+    }
+}
+
