c1a68424705451db0998b811682ba54d93336ebf
lighter <apple.xva@gmail.com>
2019-07-08 00:09:18 +0800

    Finish user login, logout, register

---
+++ ichat/SettingsTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsTableViewController.swift
+//  ichat
+//
+//  Created by lighter on 2019/7/7.
+//  Copyright © 2019 lighter. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    // MARK: IBActions
+    
+    @IBAction func logOutButtonPressed(_ sender: Any) {
+        FUser.logOutCurrentUser { (success) in
+            if success {
+                self.showLoginView()
+            }
+        }
+    }
+    
+    func showLoginView() {
+        let mainView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "welcome")
+        self.present(mainView, animated: true, completion: nil)
+    }
+}
